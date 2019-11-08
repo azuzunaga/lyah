9a79a8801e3ccc507a8841c098ed90d284233346
Americo <americo.zuzunaga@dailykos.com>
2019-11-07 16:35:56 -0800

    Example of comprehensions and tuples

--- src/Lyah/StartingOut.hs
+++ src/Lyah/StartingOut.hs
@@ -10,4 +10,20 @@ doubleSmallNumber x = if x > 100
 
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
-length' xs = sum [ 1 | _ <- xs]
+length' xs = sum [ 1 | _ <- xs ]
+
+-- Which right triangle that has integers for all sides and all sides
+-- equal to or smaller than 10 has a perimeter of 24?
+
+triangles = [ (a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a, b, c) | c <- [1..10],
+                               b <- [1..c],
+                               a <- [1..b],
+                               a^2 + b^2 == c^2 ]
+
+solution = [ (a, b, c) | c <- [1..10],
+                         b <- [1..c],
+                         a <- [1..b],
+                         a^2 + b^2 == c^2,
+                         a + b + c == 24 ]
